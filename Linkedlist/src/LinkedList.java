20d95e6d22b9cb2d4ac2f407085b9a70e6c82eb9
Abhinav Yadav <abhinavay2003@gmail.com>
2024-01-01 15:22:19 +0530

    updated

---
+++ Linkedlist/src/LinkedList.java
@@ -0,0 +1,24 @@
+public class LinkedList {
+    private  Node head;
+    private  Node tail;
+    private int size;
+
+    public LinkedList() {
+        this.size = 0;
+    }
+
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public  Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ Linkedlist/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        System.out.println(ll);
+    }
+}
